4f62ec19d8fb9fc0b86c66a0d102261fc66bb364
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2026-06-09 23:27:27 +0500

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 export API=24
 export IOS_MIN=12.0
-export NDK_PLATFORM=android-24
 
-export PREFIX=$PWD/build
+export ROOT=$PWD
+export BUILD=$PWD/build
+export DEPS=$PWD/build/deps
 export SRC=$PWD/src
