950908d320552a2ef645c1de83f91c67381b2f32
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2026-06-09 23:44:51 +0500

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 export API=24
-export IOS_MIN=12.0
+export TARGET=arm64-v8a
 
-export ROOT=$PWD
-export BUILD=$PWD/build
-export DEPS=$PWD/build/deps
-export SRC=$PWD/src
+export NDK_TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+
+export CC=$NDK_TOOLCHAIN/bin/aarch64-linux-android${API}-clang
+export CXX=$NDK_TOOLCHAIN/bin/aarch64-linux-android${API}-clang++
+export AR=$NDK_TOOLCHAIN/bin/llvm-ar
+export AS=$NDK_TOOLCHAIN/bin/llvm-as
+export LD=$NDK_TOOLCHAIN/bin/ld
+export RANLIB=$NDK_TOOLCHAIN/bin/llvm-ranlib
+
+export SYSROOT=$NDK_TOOLCHAIN/sysroot
+
+export CFLAGS="--target=aarch64-linux-android${API} --sysroot=$SYSROOT"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="$CFLAGS"
+
+export PATH=$NDK_TOOLCHAIN/bin:$PATH
