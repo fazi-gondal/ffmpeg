c7178bc242f435c266a9f03863b533eac737b5d9
Faizan gondal <146380071+fazi-gondal@users.noreply.github.com>
2026-06-09 23:10:26 +0500

    Refactor Android build dependencies script
    Updated build script for Android dependencies to include configuration for libvpx and set cross-compilation prefix.

--- scripts/01_build_deps_android.sh
+++ scripts/01_build_deps_android.sh
@@ -1,30 +1,19 @@
-set -e
+echo "Building x264..."
 
-mkdir -p src
-cd src
-
-# x264
 git clone https://code.videolan.org/videolan/x264.git
 cd x264
-./configure --host=aarch64-linux-android --enable-static --disable-cli
-make -j$(nproc)
-make install
-cd ..
 
-# x265
-git clone https://bitbucket.org/multicoreware/x265_git.git
-cd x265_git/build/linux
-cmake -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
--DANDROID_ABI=arm64-v8a \
--DANDROID_PLATFORM=24 ../../source
-make -j$(nproc)
-make install
-cd ../../..
+export CROSS_PREFIX=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 
-# libvpx
-git clone https://chromium.googlesource.com/webm/libvpx
-cd libvpx
-./configure --target=arm64-android-gcc --enable-vp9
+./configure \
+  --prefix=$PREFIX \
+  --host=aarch64-linux-android \
+  --cross-prefix=$CROSS_PREFIX \
+  --enable-static \
+  --disable-cli \
+  --extra-cflags="-march=armv8-a+simd -O3"
+
 make -j$(nproc)
 make install
+
 cd ..
